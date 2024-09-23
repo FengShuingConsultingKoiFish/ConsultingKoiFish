ef46686c4d6fc3b3b2a8b249ca77c02c27ca62bf
TamNguyen2312 <th3m1nd05@gmail.com>
2024-09-23 23:13:21 +0700

    add custom db context

---
+++ ConsultingKoiFish.DAL/ConsultingKoiFishContext.cs
@@ -0,0 +1,40 @@
+﻿using ConsultingKoiFish.DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsultingKoiFish.DAL
+{
+	public class ConsultingKoiFishContext : IdentityDbContext<IdentityUser>
+	{
+        public ConsultingKoiFishContext(DbContextOptions<ConsultingKoiFishContext> options) : base(options)
+        {
+            
+        }
+
+		#region DbSet
+
+		public virtual DbSet<UserDetail> UserDetails { get; set; }
+
+		#endregion
+
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			if (!optionsBuilder.IsConfigured)
+			{
+				var builder = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true);
+				IConfigurationRoot configuration = builder.Build();
+				optionsBuilder.UseSqlServer(configuration.GetConnectionString("OnDemandTutor"));
+			}
+		}
+	}
+}
---
+++ ConsultingKoiFish.DAL/Entities/UserDetail.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsultingKoiFish.DAL.Entities
+{
+	public class UserDetail
+	{
+		public Guid Id { get; set; }
+
+        public string FullName { get; set; } = null!;
+        public string? IdentityCard { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string? Gender { get; set; }
+        public string? Avatar { get; set; }
+        public string? Status { get; set; }
+        public bool IsLocked { get; set; }
+        public DateTime CreatedDate { get; set; }
+        //
+        public virtual IdentityUser User { get; set; }
+    }
+}
