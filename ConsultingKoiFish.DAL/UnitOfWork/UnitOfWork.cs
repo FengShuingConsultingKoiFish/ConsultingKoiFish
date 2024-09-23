ea7338f30fc9f8b913e899336d484d92a99aa87b
TamNguyen2312 <th3m1nd05@gmail.com>
2024-09-23 23:45:06 +0700

    add UnitOfWork

---
+++ ConsultingKoiFish.DAL/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,80 @@
+﻿using ConsultingKoiFish.DAL.Repositories;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsultingKoiFish.DAL.UnitOfWork
+{
+	public class UnitOfWork : IUnitOfWork
+	{
+		private readonly ConsultingKoiFishContext _context;
+		private readonly IServiceProvider _serviceProvider;
+		private IDbContextTransaction _transaction;
+		public UnitOfWork(ConsultingKoiFishContext masterContext, IServiceProvider serviceProvider)
+		{
+			_context = masterContext;
+			_serviceProvider = serviceProvider;
+		}
+		public async Task BeginTransactionAsync()
+		{
+			_transaction = await _context.Database.BeginTransactionAsync();
+		}
+
+		public async Task CommitTransactionAsync()
+		{
+			try
+			{
+				await _transaction.CommitAsync();
+			}
+			catch
+			{
+				await _transaction.RollbackAsync();
+			}
+			finally
+			{
+				await _transaction.DisposeAsync();
+				_transaction = null!;
+			}
+		}
+
+		private bool disposed = false;
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!this.disposed)
+			{
+				if (disposing)
+				{
+					_context.Dispose();
+				}
+				this.disposed = true;
+			}
+		}
+
+		public IRepoBase<T> GetRepo<T>() where T : class
+		{
+			return _serviceProvider.GetRequiredService<IRepoBase<T>>();
+		}
+
+		public async Task RollBackAsync()
+		{
+			await _transaction.RollbackAsync();
+			await _transaction.DisposeAsync();
+			_transaction = null!;
+		}
+
+		public async Task SaveChangesAsync()
+		{
+			await _context.SaveChangesAsync();
+		}
+	}
+}
