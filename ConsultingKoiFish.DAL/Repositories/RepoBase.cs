d2f7ede240ef51f81bcc08cd2a1e1ab9fd2b82b2
TamNguyen2312 <th3m1nd05@gmail.com>
2024-09-23 23:43:20 +0700

    add RepoBase

--- ConsultingKoiFish.DAL/Repositories/RepoBase.cs
+++ ConsultingKoiFish.DAL/Repositories/RepoBase.cs
@@ -1,4 +1,5 @@
 ﻿using ConsultingKoiFish.DAL.Queries;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +8,84 @@ using System.Threading.Tasks;
 
 namespace ConsultingKoiFish.DAL.Repositories
 {
-	public class RepoBase
+	public class RepoBase<T> : IRepoBase<T> where T : class
 	{
-		
+		private readonly ConsultingKoiFishContext _context;
+		protected readonly DbSet<T> _dbSet;
+		public RepoBase(ConsultingKoiFishContext context)
+		{
+			_context = context;
+			_dbSet = _context.Set<T>();
+		}
+
+
+		public async Task<T> CreateAsync(T entity)
+		{
+			await _dbSet.AddAsync(entity);
+			return entity;
+		}
+
+		public Task DeleteAsync(T entity)
+		{
+			if (_context.Entry<T>(entity).State == EntityState.Detached)
+			{
+				_dbSet.Attach(entity);
+			}
+			_dbSet.Remove(entity);
+
+			return Task.CompletedTask;
+		}
+
+		public IQueryable<T> Get(QueryOptions<T> options)
+		{
+			IQueryable<T> query = _dbSet;
+
+			if (!options.Tracked)
+			{
+				query = query.AsNoTracking();
+			}
+
+			if (options.IncludeProperties?.Any() ?? false)
+			{
+				foreach (var includeProperty in options.IncludeProperties)
+				{
+					query = query.Include(includeProperty);
+				}
+			}
+
+			if (options.Predicate != null)
+			{
+				query = query.Where(options.Predicate);
+			}
+
+			if (options.OrderBy != null)
+			{
+				query = options.OrderBy(query);
+			}
+
+			return query;
+		}
+
+		public Task UpdateAsync(T entity)
+		{
+
+			if (_context.Entry<T>(entity).State == EntityState.Detached)
+			{
+				_dbSet.Attach(entity);
+			}
+			_dbSet.Update(entity);
+
+			return Task.CompletedTask;
+		}
+
+		public async Task<IEnumerable<T>> GetAllAsync(QueryOptions<T> options)
+		{
+			return await Get(options).ToListAsync();
+		}
+
+		public async Task<T> GetSingleAsync(QueryOptions<T> options)
+		{
+			return await Get(options).FirstOrDefaultAsync();
+		}
 	}
 }
