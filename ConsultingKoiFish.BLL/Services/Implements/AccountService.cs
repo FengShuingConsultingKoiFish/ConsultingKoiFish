0674607add5a94b926cd7d2e5aff57fded7e1fe3
TamNguyen2312 <th3m1nd05@gmail.com>
2024-10-02 16:05:36 +0700

    Update AccountService.cs

--- ConsultingKoiFish.BLL/Services/Implements/AccountService.cs
+++ ConsultingKoiFish.BLL/Services/Implements/AccountService.cs
@@ -396,7 +396,7 @@ public class AccountService : IAccountService
 			Console.ForegroundColor = ConsoleColor.Red;
 			Console.WriteLine($"encode token: {decodedToken}");
 			Console.ResetColor();
-			if (decodedToken.Contains(" "))
+			if (decodedToken.Contains(' '))
 			{
 				decodedToken = decodedToken.Replace(" ", "+");
 			}
