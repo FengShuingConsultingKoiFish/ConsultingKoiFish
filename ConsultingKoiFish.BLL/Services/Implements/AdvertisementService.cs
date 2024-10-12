c4fcbc20b68f5d9261df5af160fe981374da3c0d
TamNguyen2312 <th3m1nd05@gmail.com>
2024-10-12 16:21:23 +0700

    set up for AdvertisementService

---
+++ ConsultingKoiFish.BLL/Services/Implements/AdvertisementService.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using ConsultingKoiFish.BLL.Services.Interfaces;
+using ConsultingKoiFish.DAL.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsultingKoiFish.BLL.Services.Implements
+{
+	public class AdvertisementService : IAdvertisementService
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		private readonly IMapper _mapper;
+
+		public AdvertisementService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+			this._unitOfWork = unitOfWork;
+			this._mapper = mapper;
+		}
+    }
+}
