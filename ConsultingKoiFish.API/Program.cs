86efc69735d94254698c2b972c819521fd2c3689
Đinh Nhật Hoàng <135942508+hoaq139@users.noreply.github.com>
2024-10-15 20:18:55 +0700

    Update Program.cs

--- ConsultingKoiFish.API/Program.cs
+++ ConsultingKoiFish.API/Program.cs
@@ -55,12 +55,22 @@ namespace ConsultingKoiFish.API
 
 			var app = builder.Build();
 
-			// Configure the HTTP request pipeline.
-			if (app.Environment.IsDevelopment())
-			{
-				app.UseSwagger();
-				app.UseSwaggerUI();
-			}
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "AVR API V1");
+        c.RoutePrefix = string.Empty;
+        c.EnableTryItOutByDefault();
+    });
+}
 
 			app.UseCors("corspolicy");
 			app.UseHttpsRedirection();
